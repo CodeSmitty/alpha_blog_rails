95e8fda9f98b42a46a023ef749d27971c97f4f1c
CodeSmitty <varcodesmitty@gmail.com>
2023-12-21 18:50:38 -0500

    error messages to sign up button, and redirect home sign up button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-   root "pages#home"
-    get 'about', to: 'pages#about'
-    resources :articles
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
+  get 'signup', to: 'users#new'
+  
+  resources :users, except: [:new]
 end
